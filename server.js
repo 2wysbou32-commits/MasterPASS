6f34b959111494b4cab0fdb405d4e4f8d214d981
MasterPASS <masterpass.lille@gmail.com>
2026-07-01 22:46:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -630,7 +630,7 @@ app.post('/api/login', (req, res) => {
 dbLog.connectionLogs = dbLog.connectionLogs.filter(l => (l.at || l.date) > sevenDaysAgo1);
   saveDB(dbLog);
   console.log('[SESSION] Session active enregistrée pour userId:', user.id);
-  res.json({ id: user.id, name: user.name, login: user.login, role: user.role, email: user.email || '', registeredAt: user.registeredAt || '', notifPrefs: user.notifPrefs || { announcements: true, discussions: true, files: true }, mutedThreads: user.mutedThreads || [] });
+  res.json({ id: user.id, name: user.name, login: user.login, role: user.role, email: user.email || '', registeredAt: user.registeredAt || '', notifPrefs: user.notifPrefs || { announcements: true, discussions: true, files: true, revision: true }, mutedThreads: user.mutedThreads || [] });
 });
 app.post('/api/logout', (req, res) => {
   if (req.session.userId && getActiveSessions()[req.session.userId] === req.sessionID) {
