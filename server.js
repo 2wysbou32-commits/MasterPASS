6afbee2e1aa00a2d8b57e3b6590fabb99d41464c
MasterPASS <masterpass.lille@gmail.com>
2026-08-26 20:42:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -812,7 +812,7 @@ app.post('/api/login', (req, res) => {
 dbLog.connectionLogs = dbLog.connectionLogs.filter(l => (l.at || l.date) > sevenDaysAgo1);
   saveDB(dbLog);
   console.log('[SESSION] Session active enregistrée pour userId:', user.id);
-  res.json({ id: user.id, name: user.name, login: user.login, role: user.role, email: user.email || '', registeredAt: user.registeredAt || '', notifPrefs: user.notifPrefs || { announcements: true, discussions: true, files: true, revision: true }, mutedThreads: user.mutedThreads || [] });
+    res.json({ id: user.id, name: user.name, login: user.login, role: user.role, email: user.email || '', avatar: user.avatar || null, registeredAt: user.registeredAt || '', notifPrefs: user.notifPrefs || { announcements: true, discussions: true, files: true, revision: true }, mutedThreads: user.mutedThreads || [] });
 });
 
 app.post('/api/demo-login', (req, res) => {
